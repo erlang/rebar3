85575fca65e53bdf0489ba8ea21c34dcc7cfedbc
Jan Klötzke <jan.kloetzke@freenet.de>
2011-09-29 22:42:27 +0200

    rebar_app_utils: fix git hook for Windows

--- src/rebar_app_utils.erl
+++ src/rebar_app_utils.erl
@@ -171,7 +171,14 @@ vcs_vsn_cmd(git) ->
     %% Explicitly git-describe a committish to accommodate for projects
     %% in subdirs which don't have a GIT_DIR. In that case we will
     %% get a description of the last commit that touched the subdir.
-    "git describe --always --tags `git log -n 1 --pretty=format:%h .`";
+    case os:type() of
+        {win32,nt} ->
+            "FOR /F \"usebackq tokens=* delims=\" %i in "
+            "(`git log -n 1 \"--pretty=format:%h\" .`) do "
+            "@git describe --always --tags %i";
+        _ ->
+            "git describe --always --tags `git log -n 1 --pretty=format:%h .`"
+    end;
 vcs_vsn_cmd(hg)  -> "hg identify -i";
 vcs_vsn_cmd(bzr) -> "bzr revno";
 vcs_vsn_cmd(svn) -> "svnversion";
