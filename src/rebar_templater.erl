erlang/rebar3

commit 4414f6548ac60b8083f00e5da267198e9d842558

List substitution variable names in 'list-templates' command.

src/rebar_templater.erl
@@ -64,7 +64,11 @@
     ?CONSOLE("Available templates:\n", []),
     _ = [begin
              BaseName = filename:basename(F, ".template"),
-             ?CONSOLE("\t* ~s: ~s (~p)\n", [BaseName, F, Type])
+             {ok, Template} = file:consult(F),
+             {_, VarList} = lists:keyfind(variables, 1, Template),
+             Vars = lists:foldl(fun({V,_}, Acc) -> [atom_to_list(V)|Acc] end, [], VarList),
+             ?CONSOLE("\t* ~s: ~s (~p) (variables: ~p)\n", [BaseName, F, Type, 
+                                                            string:join(Vars, ", ")])
          end || {Type, F} <- AvailTemplates],
     ok.
 

commit 49f3cc4108d0dcc70f74ed02edf4d933c6fbfdb0

Fix crash introduced in 4414f65 via pull/245

src/rebar_templater.erl
@@ -64,8 +64,8 @@
     ?CONSOLE("Available templates:\n", []),
     _ = [begin
              BaseName = filename:basename(F, ".template"),
-             {ok, Template} = file:consult(F),
-             {_, VarList} = lists:keyfind(variables, 1, Template),
+             TemplateTerms = consult(load_file(Type, F)),
+             {_, VarList} = lists:keyfind(variables, 1, TemplateTerms),
              Vars = lists:foldl(fun({V,_}, Acc) ->
                                         [atom_to_list(V) | Acc]
                                 end, [], VarList),