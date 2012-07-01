3da58e22728d3a09cf995dbc518b9b72c2a38d3d
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2012-07-01 22:21:26 +0200

    Fix test: inttest/t_custom_config

--- inttest/t_custom_config/t_custom_config_rt.erl
+++ inttest/t_custom_config/t_custom_config_rt.erl
@@ -11,7 +11,7 @@ files() ->
 
 run(Dir) ->
     retest_log:log(debug, "Running in Dir: ~s~n", [Dir]),
-    Ref = retest:sh("./rebar -C custom.config check-deps -v", [{async, true}]),
+    Ref = retest:sh("./rebar -C custom.config check-deps -vvv", [{async, true}]),
     {ok, Captured} =
         retest:sh_expect(Ref,
                          "DEBUG: Consult config file .*/custom.config.*",
