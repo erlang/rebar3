1b52a597c44209da3903e1477399ea35a6a9efc4
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2012-09-21 17:20:03 +0200

    Revert 15b7798e4 and restore old git-describe call
    Restore previous 'git describe' behaviour as discussed here:
    http://lists.basho.com/pipermail/rebar_lists.basho.com/2012-September/001713.html

--- src/rebar_utils.erl
+++ src/rebar_utils.erl
@@ -475,17 +475,7 @@ vcs_vsn_1(Vcs, Dir) ->
             end
     end.
 
-vcs_vsn_cmd(git) ->
-    %% git describe the last commit that touched CWD
-    %% required for correct versioning of apps in subdirs, such as apps/app1
-    case os:type() of
-        {win32,nt} ->
-            "FOR /F \"usebackq tokens=* delims=\" %i in "
-                "(`git log -n 1 \"--pretty=format:%h\" .`) do "
-                "@git describe --always --tags %i";
-        _ ->
-            "git describe --always --tags `git log -n 1 --pretty=format:%h .`"
-    end;
+vcs_vsn_cmd(git) -> "git describe --always --tags";
 vcs_vsn_cmd(hg)  -> "hg identify -i";
 vcs_vsn_cmd(bzr) -> "bzr revno";
 vcs_vsn_cmd(svn) -> "svnversion";
