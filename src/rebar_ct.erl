erlang/rebar3

commit bcc57e9b044678ab8769dd7e4272a137a0426a90

Handle ct_run exit codes in R15B02 and later
Since R15B02, ct_run returns a non-zero exit code when some tests
failed or were auto-skipped. (See ticket OTP-9865.)

This fix makes it so a non-0 code doesn't cause an instant failure, but
still prompts for log verification before doing so. Given the behaviour
was acceptable for pre-R15B02, it should be valid with it with
post-R15B02.

The fix should also be backwards compatible.

src/rebar_ct.erl
@@ -108,8 +108,16 @@ run_test(TestDir, LogDir, Config, _File) ->
                      " 2>&1 | tee -a " ++ RawLog
              end,
 
-    rebar_utils:sh(Cmd ++ Output, [{env,[{"TESTDIR", TestDir}]}]),
-    check_log(Config, RawLog).
+    case rebar_utils:sh(Cmd ++ Output, [{env,[{"TESTDIR", TestDir}]}, return_on_error]) of
+        {ok,_} ->
+            %% in older versions of ct_run, this could have been a failure
+            %% that returned a non-0 code. Check for that!
+            check_success_log(Config, RawLog);
+        {error,Res} ->
+            %% In newer ct_run versions, this may be a sign of a good compile
+            %% that failed cases. In older version, it's a worse error.
+            check_fail_log(Config, RawLog, Cmd ++ Output, Res)
+    end.
 
 clear_log(LogDir, RawLog) ->
     case filelib:ensure_dir(filename:join(LogDir, "index.html")) of
@@ -124,7 +132,16 @@ clear_log(LogDir, RawLog) ->
 
 %% calling ct with erl does not return non-zero on failure - have to check
 %% log results
-check_log(Config, RawLog) ->
+check_success_log(Config, RawLog) ->
+    check_log(Config, RawLog, fun(Msg) -> ?CONSOLE("DONE.\n~s\n", [Msg]) end).
+
+check_fail_log(Config, RawLog, Command, {Rc, Output}) ->
+    check_log(Config, RawLog, fun(_Msg) ->
+            ?ABORT("~s failed with error: ~w and output:~n~s~n",
+                   [Command, Rc, Output])
+    end).
+
+check_log(Config,RawLog,Fun) ->
     {ok, Msg} =
         rebar_utils:sh("grep -e 'TEST COMPLETE' -e '{error,make_failed}' "
                        ++ RawLog, [{use_stdout, false}]),
@@ -142,8 +159,9 @@ check_log(Config, RawLog) ->
             ?FAIL;
 
         true ->
-            ?CONSOLE("DONE.\n~s\n", [Msg])
+            Fun(Msg)
     end.
+
 
 %% Show the log if it hasn't already been shown because verbose was on
 show_log(Config, RawLog) ->

commit edd835510b62c88bdc451839f1a34ceeb06cbd66

rebar_ct: fix Dialyzer warning caused by bcc57e9b
Also fix a single line exceeding max column.

src/rebar_ct.erl
@@ -108,7 +108,8 @@ run_test(TestDir, LogDir, Config, _File) ->
                      " 2>&1 | tee -a " ++ RawLog
              end,
 
-    case rebar_utils:sh(Cmd ++ Output, [{env,[{"TESTDIR", TestDir}]}, return_on_error]) of
+    ShOpts = [{env,[{"TESTDIR", TestDir}]}, return_on_error],
+    case rebar_utils:sh(Cmd ++ Output, ShOpts) of
         {ok,_} ->
             %% in older versions of ct_run, this could have been a failure
             %% that returned a non-0 code. Check for that!
@@ -135,11 +136,16 @@ clear_log(LogDir, RawLog) ->
 check_success_log(Config, RawLog) ->
     check_log(Config, RawLog, fun(Msg) -> ?CONSOLE("DONE.\n~s\n", [Msg]) end).
 
-check_fail_log(Config, RawLog, Command, {Rc, Output}) ->
-    check_log(Config, RawLog, fun(_Msg) ->
+-type err_handler() :: fun((string()) -> no_return()).
+-spec failure_logger(string(), {integer(), string()}) -> err_handler().
+failure_logger(Command, {Rc, Output}) ->
+    fun(_Msg) ->
             ?ABORT("~s failed with error: ~w and output:~n~s~n",
                    [Command, Rc, Output])
-    end).
+    end.
+
+check_fail_log(Config, RawLog, Command, Result) ->
+    check_log(Config, RawLog, failure_logger(Command, Result)).
 
 check_log(Config,RawLog,Fun) ->
     {ok, Msg} =