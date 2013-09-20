erlang/rebar3

commit 2716d83a189cdbe48f8dc5b33a25e887f672ad62

Fix basho/rebar/issues/286

src/rebar_eunit.erl
@@ -762,7 +762,6 @@ pause_until_net_kernel_stopped(0) ->
     exit(net_kernel_stop_failed);
 pause_until_net_kernel_stopped(N) ->
     try
-        _ = net_kernel:i(),
         timer:sleep(100),
         pause_until_net_kernel_stopped(N - 1)
     catch

commit 0550560221fde6d5c19bec472e395fb20d720252

Fix eunit regression introduced in 2716d83a18
Use alternative way to detect net_kernel:stop/0 has succeeded as
suggested by Ulf Wiger.

src/rebar_eunit.erl
@@ -802,11 +802,11 @@ pause_until_net_kernel_stopped() ->
 pause_until_net_kernel_stopped(0) ->
     exit(net_kernel_stop_failed);
 pause_until_net_kernel_stopped(N) ->
-    try
-        timer:sleep(100),
-        pause_until_net_kernel_stopped(N - 1)
-    catch
-        error:badarg ->
+    case node() of
+        'nonode@nohost' ->
             ?DEBUG("Stopped net kernel.\n", []),
-            ok
+            ok;
+        _ ->
+            timer:sleep(100),
+            pause_until_net_kernel_stopped(N - 1)
     end.