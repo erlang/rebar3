23c63f7003c3f80fddc8580cfcf208aa0639b80c
Slava Yurin <v.yurin@office.ngs.ru>
2014-05-28 14:05:26 +0700

    Regression test for #249

--- inttest/erlc/extra-src/foo_sup.erl
+++ inttest/erlc/extra-src/foo_sup.erl
@@ -1,3 +1,5 @@
+%% -*- mode: erlang;erlang-indent-level: 4;indent-tabs-mode: nil -*-
+%% ex: ts=4 sw=4 ft=erlang et
 -module(foo_sup).
 
 -behavior(supervisor).
---
+++ inttest/erlc/src/first_erl.erl
@@ -0,0 +1,10 @@
+%% -*- mode: erlang;erlang-indent-level: 4;indent-tabs-mode: nil -*-
+%% ex: ts=4 sw=4 ft=erlang et
+-module(first_erl).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-export([test/0]).
+
+test() ->
+    ?debugHere.
