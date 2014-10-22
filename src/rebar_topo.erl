bd1501f079aea3d2a991343224f2b9fc472f5d5f
Tristan Sloughter <t@crashfast.com>
2014-10-22 12:03:34 -0500

    fix format_error cycle

--- src/rebar_topo.erl
+++ src/rebar_topo.erl
@@ -76,10 +76,10 @@ format_error({cycle, Pairs}) ->
      "before we can continue:\n",
     case Pairs of
         [{P1, P2}] ->
-            [rebar_utils:indent(2), P2, "->", P1];
+            [rebar_utils:indent(2), erlang:atom_to_list(P2), "->", erlang:atom_to_list(P1)];
         [{P1, P2} | Rest] ->
-            [rebar_utils:indent(2), P2, "->", P1,
-             [["-> ", PP2, " -> ", PP1] || {PP1, PP2} <- Rest]];
+            [rebar_utils:indent(2), erlang:atom_to_list(P2), "->", erlang:atom_to_list(P1),
+             [["-> ", erlang:atom_to_list(PP2), " -> ", erlang:atom_to_list(PP1)] || {PP1, PP2} <- Rest]];
         [] ->
             []
     end].
