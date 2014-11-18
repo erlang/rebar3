71de52cb9d3659da644e4c592d7090b10fc73d2c
Tristan Sloughter <t@crashfast.com>
2014-11-18 08:48:59 -0600

    fix callback r14 support

--- src/rebar_resource.erl
+++ src/rebar_resource.erl
@@ -14,7 +14,7 @@
 -type location() :: string().
 -type ref() :: any().
 
--ifdef(have_callback_support).
+-ifdef(no_callback_support).
 
 %% In the case where R14 or lower is being used to compile the system
 %% we need to export a behaviour info
