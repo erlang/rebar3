fb630d18ea98af6b91f263cdf8c14185454edf04
Tristan Sloughter <t@crashfast.com>
2014-11-19 18:19:27 -0600

    add included applications to app_info applications element

--- src/rebar_app_discover.erl
+++ src/rebar_app_discover.erl
@@ -120,6 +120,7 @@ create_app_info(AppDir, AppFile) ->
         {ok, [{application, AppName, AppDetails}]} ->
             AppVsn = proplists:get_value(vsn, AppDetails),
             Applications = proplists:get_value(applications, AppDetails, []),
+            IncludedApplications = proplists:get_value(included_applications, AppDetails, []),
             C = rebar_config:consult(AppDir),
             S = rebar_state:new(rebar_state:new(), C, AppDir),
             AppDeps = rebar_state:deps_names(S),
@@ -136,7 +137,7 @@ create_app_info(AppDir, AppFile) ->
             AppState1 = rebar_state:set(AppState, base_dir, AbsCwd),
             AppInfo1 = rebar_app_info:applications(rebar_app_info:config(
                                                      rebar_app_info:app_details(AppInfo, AppDetails)
-                                                     ,AppState1), Applications),
+                                                     ,AppState1), IncludedApplications++Applications),
             rebar_app_info:dir(AppInfo1, AppDir);
         _ ->
             error
