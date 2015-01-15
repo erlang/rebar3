00e03d70efa697bcd6cba53d62efc461035a4d82
alisdair sullivan <alisdairsullivan@yahoo.ca>
2015-01-15 09:48:18 -0800

    use new `rebar_file_utils` functions in `eunit` and `ct` providers

--- src/rebar_prv_common_test.erl
+++ src/rebar_prv_common_test.erl
@@ -182,19 +182,11 @@ split_ct_dirs(State, RawOpts) ->
     {InDirs, OutDir}.
 
 default_test_dir(State) ->
-    Tmp = case erlang:system_info(system_architecture) of
-        "win32" ->
-            "./tmp";
-        _SysArch ->
-            "/tmp"
-    end,
+    Tmp = rebar_file_utils:system_tmpdir(),
     Root = filename:join([rebar_state:dir(State), Tmp]),
     Project = filename:basename(rebar_state:dir(State)),
     OutDir = filename:join([Root, Project ++ "_rebar3_ct"]),
-    %% delete the directory if it exists so tests run with clean state
-    _ = ec_file:remove(OutDir, [recursive]),
-    %% recreate the directory
-    ok = filelib:ensure_dir(filename:join([OutDir, "dummy.beam"])),
+    ok = rebar_file_utils:reset_dir(OutDir),
     OutDir.
 
 transform_opts(Opts) ->
