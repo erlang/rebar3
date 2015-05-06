ec600a5b8580c22bba4081ccdfc5fdcd2d345f11
Tristan Sloughter <t@crashfast.com>
2015-05-05 19:12:11 -0500

    add license

--- src/rebar_mustache.erl
+++ src/rebar_mustache.erl
@@ -1,6 +1,18 @@
-%% @copyright 2015 Hinagiku Soranoba All Rights Reserved.
+%% The MIT License (MIT)
 %%
-%% @doc Mustach template engine for Erlang/OTP.
+%% Copyright (c) 2015 Hinagiku Soranoba
+%%
+%% Permission is hereby granted, free of charge, to any person obtaining a copy
+%% of this software and associated documentation files (the "Software"), to deal
+%% in the Software without restriction, including without limitation the rights
+%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%% copies of the Software, and to permit persons to whom the Software is
+%% furnished to do so, subject to the following conditions:
+%%
+%% The above copyright notice and this permission notice shall be included in all
+%% copies or substantial portions of the Software.
+%%
+%% @doc Mustache template engine for Erlang/OTP.
 -module(rebar_mustache).
 
 %%----------------------------------------------------------------------------------------------------------------------
