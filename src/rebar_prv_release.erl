33e0612a75d0cc7f1e023c897af305bc4b00faa6
Tristan Sloughter <t@crashfast.com>
2015-05-31 15:50:14 -0500

    run pre and post hooks for release and tar providers

--- src/rebar_prv_release.erl
+++ src/rebar_prv_release.erl
@@ -40,6 +40,9 @@ do(State) ->
                                    [?DEFAULT_CHECKOUTS_DIR, DepsDir | ProjectAppDirs]),
     OutputDir = filename:join(rebar_dir:base_dir(State), ?DEFAULT_RELEASE_DIR),
     AllOptions = string:join(["release" | Options], " "),
+    Cwd = rebar_state:dir(State),
+    Providers = rebar_state:providers(State),
+    rebar_hooks:run_all_hooks(Cwd, pre, ?PROVIDER, Providers, State),
     try
         case rebar_state:get(State, relx, []) of
             [] ->
@@ -52,6 +55,7 @@ do(State) ->
                           ,{output_dir, OutputDir}
                           ,{caller, Caller}], AllOptions)
         end,
+        rebar_hooks:run_all_hooks(Cwd, post, ?PROVIDER, Providers, State),
         {ok, State}
     catch
         throw:T ->
