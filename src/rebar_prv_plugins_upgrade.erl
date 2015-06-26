c2209785f96b8de2fd3a77017a038711be2d6cc0
Tristan Sloughter <t@crashfast.com>
2015-06-25 21:17:05 -0500

    add error message when no plugin is passed to plugins upgrade

--- src/rebar_prv_plugins_upgrade.erl
+++ src/rebar_prv_plugins_upgrade.erl
@@ -33,10 +33,16 @@ init(State) ->
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
     {Args, _} = rebar_state:command_parsed_args(State),
-    Plugin = proplists:get_value(plugin, Args, <<"">>),
-    upgrade(Plugin, State).
+    case proplists:get_value(plugin, Args, none) of
+        none ->
+            ?PRV_ERROR(no_plugin_arg);
+        Plugin ->
+            upgrade(Plugin, State)
+    end.
 
 -spec format_error(any()) -> iolist().
+format_error(no_plugin_arg) ->
+    io_lib:format("Must give an installed plugin to upgrade as an argument", []);
 format_error({not_found, Plugin}) ->
     io_lib:format("Plugin to upgrade not found: ~s", [Plugin]);
 format_error(Reason) ->
