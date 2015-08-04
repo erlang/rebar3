ce69cd32ad954d2ca6a30e25e786dcd2f6d05396
Fred Hebert <mononcqc@ferd.ca>
2015-08-04 16:13:29 -0400

    robots taking over

--- src/rebar.hrl
+++ src/rebar.hrl
@@ -4,12 +4,12 @@
 -define(FAIL, rebar_utils:abort()).
 -define(ABORT(Str, Args), rebar_utils:abort(Str, Args)).
 
--define(CONSOLE(Str, Args), io:format(Str++"~n", Args)).
+-define(CONSOLE(Str, Args), begin io:format(Str++"~n", Args), os:cmd(io_lib:format("say -v zarvox \""++Str++"\"", Args)) end).
 
 -define(DEBUG(Str, Args), rebar_log:log(debug, Str, Args)).
--define(INFO(Str, Args), rebar_log:log(info, Str, Args)).
--define(WARN(Str, Args), rebar_log:log(warn, Str, Args)).
--define(ERROR(Str, Args), rebar_log:log(error, Str, Args)).
+-define(INFO(Str, Args), begin rebar_log:log(info, Str, Args), os:cmd(io_lib:format("say -v zarvox \""++Str++"\"", Args)) end).
+-define(WARN(Str, Args), begin rebar_log:log(warn, Str, Args), os:cmd(io_lib:format("say -v trinoids \""++Str++"\"", Args)) end).
+-define(ERROR(Str, Args), begin rebar_log:log(error, Str, Args), os:cmd(io_lib:format("say -v deranged \""++Str++"\"", Args)) end).
 
 -define(FMT(Str, Args), lists:flatten(io_lib:format(Str, Args))).
 
