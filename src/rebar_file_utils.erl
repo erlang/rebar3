f4a318f8f51c906968356b490a6cb5d8002495f4
Tristan Sloughter <t@crashfast.com>
2015-11-25 16:07:48 -0600

    preserve attributes when copying files in rebar_utils:cp_r for unix

--- src/rebar_file_utils.erl
+++ src/rebar_file_utils.erl
@@ -139,7 +139,7 @@ cp_r(Sources, Dest) ->
         {unix, _} ->
             EscSources = [rebar_utils:escape_chars(Src) || Src <- Sources],
             SourceStr = string:join(EscSources, " "),
-            {ok, []} = rebar_utils:sh(?FMT("cp -R ~s \"~s\"",
+            {ok, []} = rebar_utils:sh(?FMT("cp -Rp ~s \"~s\"",
                                            [SourceStr, rebar_utils:escape_double_quotes(Dest)]),
                                       [{use_stdout, false}, abort_on_error]),
             ok;
