4c2873d213ccdb4182f21c7179038897a66210b2
Rasmus Svensson <raek@raek.se>
2016-08-15 21:20:14 +0200

    Print error on too many help arguments
    Previously the help task would crash when given more than two
    arguments. After this change it instead print a message:
    
        Too many arguments given. Usage: rebar3 help [<namespace>] <task>

--- src/rebar_prv_help.erl
+++ src/rebar_prv_help.erl
@@ -41,7 +41,10 @@ do(State) ->
         [Name] -> % default namespace
             task_help(default, list_to_atom(Name), State);
         [Namespace, Name] ->
-            task_help(list_to_atom(Namespace), list_to_atom(Name), State)
+            task_help(list_to_atom(Namespace), list_to_atom(Name), State);
+        _ ->
+            {error, "Too many arguments given. " ++
+                 "Usage: rebar3 help [<namespace>] <task>"}
     end.
 
 -spec format_error(any()) -> iolist().
