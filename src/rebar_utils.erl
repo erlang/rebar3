e71b80752f905d9f4bb5dbf78c16693dbc6133f4
Alexander Sedov <asedov@platbox.com>
2016-10-11 16:34:21 +0300

    Some post-review changes:
    - restore path after loading applications,
    - helpful comments.

--- src/rebar_utils.erl
+++ src/rebar_utils.erl
@@ -414,6 +414,8 @@ user_agent() ->
     ?FMT("Rebar/~s (OTP/~s)", [Vsn, otp_release()]).
 
 reread_config(ConfigList) ->
+    %% NB: we attempt to mimic -config here, which survives app reload,
+    %% hence {persistent, true}.
     SetEnv = case version_tuple(?MODULE:otp_release()) of
         {X, _, _} when X =< 17 ->
             fun application:set_env/3;
