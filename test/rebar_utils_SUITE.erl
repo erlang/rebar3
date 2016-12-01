2c155ead23abeedb39ff761e4db5269f7b2a78ca
Fred Hebert <mononcqc@ferd.ca>
2016-12-01 11:47:05 -0500

    Fully clean up after test utils for proxy

--- test/rebar_utils_SUITE.erl
+++ test/rebar_utils_SUITE.erl
@@ -303,9 +303,10 @@ proxy_auth(_Config, ProxyEnvKey) ->
 				 rebar_utils:get_proxy_auth()),
 
 	%% restore original proxy specification if any
-	restore_proxy_env(ProxyEnvKey, OldProxySpec).
+	restore_proxy_env(ProxyEnvKey, OldProxySpec),
+	application:unset_env(rebar, proxy_auth).
 
-restore_proxy_env(_, false) ->
-	ok;
+restore_proxy_env(ProxyEnvKey, false) ->
+    os:putenv(ProxyEnvKey, "");
 restore_proxy_env(ProxyEnvKey, ProxySpec) ->
-	os:putenv(ProxyEnvKey, ProxySpec).	
+    os:putenv(ProxyEnvKey, ProxySpec).
