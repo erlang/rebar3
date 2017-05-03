1f9404e9cd5edc44627a17204c9ba01b32487dae
Fred Hebert <mononcqc@ferd.ca>
2017-05-03 08:36:11 -0400

    Drop R15 support
    It can't even fetch packages from Hex anymore because of old SSL/TLS
    libraries, and so it can't bootstrap anymore either. Plus R20 comes out
    soon, and 5 major versions is quite enough.

--- src/rebar_prv_common_test.erl
+++ src/rebar_prv_common_test.erl
@@ -567,9 +567,6 @@ get_tests_from_specs(Specs) ->
             case ct_testspec:collect_tests_from_file(Specs,true) of
                 Tests when is_list(Tests) ->
                     {ok,[{S,ct_testspec:prepare_tests(R)} || {S,R} <- Tests]};
-                R when is_tuple(R), element(1,R)==testspec ->
-                    %% R15
-                    {ok,[{Specs,ct_testspec:prepare_tests(R)}]};
                 Error ->
                     Error
             end
