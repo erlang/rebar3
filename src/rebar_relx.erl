00d36c57be98b4e9a2971b2fd97fd779221c94d1
tothlac <tothlac@gmail.com>
2018-10-10 16:59:37 +0200

    1910: don't pass {base_dir, Basedir} to relx but Basedir

--- src/rebar_relx.erl
+++ src/rebar_relx.erl
@@ -40,7 +40,7 @@ do(Module, Command, Provider, State) ->
                           ,{caller, api}
                           ,{log_level, LogLevel} | output_dir(OutputDir, Options)] ++ ErlOpts, AllOptions);
             Config ->
-                Config1 = [{overlay_vars, [{base_dir, rebar_dir:base_dir(State)}]}
+                Config1 = [{overlay_vars, [rebar_dir:base_dir(State)]}
                            | merge_overlays(Config)],
                 relx:main([{lib_dirs, LibDirs}
                           ,{config, Config1}
