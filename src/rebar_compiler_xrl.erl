0581ab72c46252bf44d964ac47497638df5672d0
Tristan Sloughter <t@crashfast.com>
2018-11-23 13:48:57 -0700

    fix base path used for yrl/xrl includefile configs

--- src/rebar_compiler_xrl.erl
+++ src/rebar_compiler_xrl.erl
@@ -8,6 +8,8 @@
          compile/4,
          clean/2]).
 
+-export([update_opts/2]).
+
 context(AppInfo) ->
     Dir = rebar_app_info:dir(AppInfo),
     Mappings = [{".erl", filename:join([Dir, "src"])}],
@@ -25,28 +27,38 @@ needed_files(_, FoundFiles, Mappings, AppInfo) ->
                            rebar_compiler:needs_compile(Source, ".erl", Mappings)],
 
     Opts = rebar_opts:get(rebar_app_info:opts(AppInfo), xrl_opts, []),
+    Opts1 = update_opts(Opts, AppInfo),
 
-    {{FirstFiles, Opts}, {RestFiles, Opts}}.
+    {{FirstFiles, Opts1}, {RestFiles, Opts1}}.
 
 dependencies(_, _, _) ->
     [].
 
-compile(Source, [{_, OutDir}], _, Opts) ->
-    BaseName = filename:basename(Source),
-    Target = filename:join([OutDir, BaseName]),
-    AllOpts = [{parserfile, Target} | Opts],
-    AllOpts1 = [{includefile, filename:join(OutDir, I)} || {includefile, I} <- AllOpts,
-                                                           filename:pathtype(I) =:= relative],
-    case leex:file(Source, AllOpts1 ++ [{return, true}]) of
+compile(Source, [{_, _}], _, Opts) ->
+    case leex:file(Source, [{return, true} | Opts]) of
         {ok, _} ->
             ok;
         {ok, _Mod, Ws} ->
             rebar_compiler:ok_tuple(Source, Ws);
         {error, Es, Ws} ->
-            rebar_compiler:error_tuple(Source, Es, Ws, AllOpts1)
+            rebar_compiler:error_tuple(Source, Es, Ws, Opts)
     end.
 
 clean(XrlFiles, _AppInfo) ->
     rebar_file_utils:delete_each(
       [rebar_utils:to_list(re:replace(F, "\\.xrl$", ".erl", [unicode]))
        || F <- XrlFiles]).
+
+%% make includefile options absolute paths
+update_opts(Opts, AppInfo) ->
+    OutDir = rebar_app_info:out_dir(AppInfo),
+    lists:map(fun({includefile, I}) ->
+                      case filename:pathtype(I) =:= relative of
+                          true ->
+                              {includefile, filename:join(OutDir, I)};
+                          false ->
+                              {includefile, I}
+                      end;
+                 (O) ->
+                      O
+              end, Opts).
