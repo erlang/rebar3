235d5792bf6978e17cc323f01944d448e1969595
Fred Hebert <mononcqc@ferd.ca>
2023-01-23 00:45:48 +0000

    Add rebar_parallel pool, use in DAG scans
    This addresses https://github.com/erlang/rebar3/issues/2767 by creating
    a pool mechanism in rebar_parallel that keeps as similar of an interface
    as possible as the queue mechanism, with the one caveat that it allows
    the asynchronous creation of tasks rather than requiring them all at
    start time.
    
    The mechanism is not tested super deeply, which is probably a mistake,
    but I wanted to get a reviewable PR first.
    
    The mechanism is also added to the rebar_compiler_dag module to cover
    use cases that were handled by spawning an unbounded number of processes
    before, which would cause problem with low file descriptors being
    allocated and lots of files being used and open in parallel. The pool
    mechanism puts an upper bound on processing but also on resource usage.
    
    So this PR may also come with a performance regression, and if so we'd
    want to override the default 1-per-scheduler pool options to use a lot
    more and hit a middleground in performance vs. resource usage.

--- apps/rebar/src/rebar_parallel.erl
+++ apps/rebar/src/rebar_parallel.erl
@@ -7,7 +7,8 @@
 %%% and extracted here to be reused in other circumstances.
 %%% @end
 -module(rebar_parallel).
--export([queue/5]).
+-export([queue/5,
+         pool/4, pool/5, pool_task/2, pool_terminate/1]).
 -include("rebar.hrl").
 
 queue(Tasks, WorkF, WArgs, Handler, HArgs) ->
@@ -17,6 +18,36 @@ queue(Tasks, WorkF, WArgs, Handler, HArgs) ->
     ?DIAGNOSTIC("Starting ~B worker(s)", [Jobs]),
     Pids = [spawn_monitor(Worker) || _ <- lists:seq(1, Jobs)],
     parallel_dispatch(Tasks, Pids, Handler, HArgs).
+
+pool(WorkF, WArgs, Handler, HArgs) ->
+    pool(WorkF, WArgs, Handler, HArgs, erlang:system_info(schedulers)).
+
+pool(WorkF, WArgs, Handler, HArgs, Jobs) ->
+    Parent = self(),
+    Coordinator = spawn_link(fun() ->
+        Coord = self(),
+        Worker = fun() -> worker(Coord, WorkF, WArgs) end,
+        Pids = [spawn_monitor(Worker) || _ <- lists:seq(1, Jobs)],
+        Parent ! pool_ready,
+        pool_coordinator([], [], Pids, Handler, HArgs, [], undefined)
+    end),
+    receive pool_ready -> ok end,
+    Coordinator.
+
+pool_task(Pid, Task) ->
+    Pid ! {task, Task},
+    ok.
+
+pool_terminate(Pid) ->
+    Ref = erlang:monitor(process, Pid),
+    Pid ! {self(), terminate},
+    receive
+        {Pid, Res} ->
+            erlang:demonitor(Ref, [flush]),
+            Res;
+        {'DOWN', Ref, process, Pid, Info} ->
+            error(Info)
+    end.
 
 parallel_dispatch([], [], _, _) ->
     [];
@@ -52,5 +83,56 @@ worker(QueuePid, F, Args) ->
             worker(QueuePid, F, Args);
         empty ->
             ok
+    end.
+
+pool_coordinator(Tasks, FreePids, Pids, Handler, HArgs, Acc, Report) ->
+    receive
+        {task, Task} when Report =/= undefined ->
+            ?ERROR("Task added to pool after being terminated: ~p", [Task]),
+            ?ABORT;
+        {task, Task} when FreePids =:= [] ->
+            pool_coordinator([Task|Tasks], FreePids, Pids, Handler, HArgs, Acc, Report);
+        {task, Task} ->
+            [Pid|NewFree] = FreePids,
+            Pid ! {task, Task},
+            pool_coordinator(Tasks, NewFree, Pids, Handler, HArgs, Acc, Report);
+        {ready, _Pid} when Tasks =:= [], Report =/= undefined ->
+            %% And we're done
+            %% terminate workers async, return results if done
+            [Pid ! empty || {Pid,_Ref} <- Pids],
+            pool_coordinator(Tasks, FreePids, Pids, Handler, HArgs, Acc, Report);
+        {ready, Pid} when Tasks =:= [] ->
+            pool_coordinator(Tasks, [Pid|FreePids], Pids, Handler, HArgs, Acc, Report);
+        {ready, Pid} ->
+            [Task|NewTasks] = Tasks,
+            Pid ! {task, Task},
+            pool_coordinator(NewTasks, FreePids, Pids, Handler, HArgs, Acc, Report);
+        {'DOWN', Mref, _, Pid, normal} ->
+            NewPids = lists:delete({Pid, Mref}, Pids),
+            NewFree = lists:delete(Pid, FreePids),
+            case NewPids of
+                [] when is_pid(Report) ->
+                    Report ! {self(), Acc};
+                _ ->
+                    pool_coordinator(Tasks, NewFree, NewPids, Handler, HArgs, Acc, Report)
+            end;
+        {'DOWN', _Mref, _, _Pid, Info} ->
+            ?ERROR("Task failed: ~p", [Info]),
+            ?ABORT;
+        {result, Result} ->
+            case Handler(Result, HArgs) of
+                ok ->
+                    pool_coordinator(Tasks, FreePids, Pids, Handler, HArgs, Acc, Report);
+                {ok, Res} ->
+                    pool_coordinator(Tasks, FreePids, Pids, Handler, HArgs, [Res|Acc], Report)
+            end;
+        {Caller, terminate} ->
+            if Pids =:= [];                                       % no workers somehow
+               length(Pids) =:= length(FreePids), Tasks =:= [] -> % All Idle
+                    Caller ! {self(), Acc};
+               true ->
+                   %% Still work to do
+                   pool_coordinator(Tasks, FreePids, Pids, Handler, HArgs, Acc, Caller)
+            end
     end.
 
